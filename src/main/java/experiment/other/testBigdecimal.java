660ad53581959c8f070994fffb053c413854ddb2
zengjiaping <cdzengjiaping@jd.com>
2017-03-01 18:32:44 +0800

    bigdicemal

---
+++ src/main/java/experiment/other/testBigdecimal.java
@@ -0,0 +1,13 @@
+package experiment.other;
+
+import java.math.BigDecimal;
+
+public class testBigdecimal {
+	public static void main(String[] args) {
+		BigDecimal b1 = new BigDecimal(1.0);
+		BigDecimal b2 = new BigDecimal(0);
+		BigDecimal b3 = new BigDecimal(0.0);
+		System.out.println(b1.compareTo(b2) == 0 ? "true" : "false");
+		System.out.println(b3.compareTo(b2) == 0 ? "true" : "false");
+	}
+}
