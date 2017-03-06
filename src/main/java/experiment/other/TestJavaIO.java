5af3972654947bd74ff4bf6eac9dd467133b58c4
zengjiaping <cdzengjiaping@jd.com>
2017-03-06 13:37:07 +0800

    fix

---
+++ src/main/java/experiment/other/TestJavaIO.java
@@ -0,0 +1,81 @@
+package experiment.other;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+
+/**
+ * @Author japing
+ * @Date 2017/3/6 10:53
+ * @Description:
+ */
+public class TestJavaIO {
+
+    public static void main(String[] args) {
+        try {
+            if (null != args) {
+                printFile(args[0]);
+            }
+            System.out.println("======================================================");
+            if (null != args) {
+                printFile2(args[0]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static  void printFile(String args) throws Exception{
+        File file = new File(args);
+        File[] files = file.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+
+                return true;
+            }
+        });
+
+        for (File item : files) {
+            if (item.isFile()) {
+                System.out.println(item.getAbsolutePath());
+                System.out.println(item.getCanonicalPath());
+                System.out.println(item.getPath());
+                System.out.println(item.getName());
+            } else {
+                System.out.println(item.getAbsolutePath());
+                System.out.println(item.getCanonicalPath());
+                System.out.println(item.getPath());
+                System.out.println(item.getName());
+                String subpath = args + "\\" + item.getName();
+                printFile(subpath);
+            }
+        }
+    }
+
+    public static void printFile2(String path) {
+       try {
+           String pathStr = "experiment.other";
+           String pathStr2 = pathStr.replaceAll("\\.", "/");
+           Enumeration<URL> enumerations = getClassLoader().getResources(("experiment/other"));
+           if (null != enumerations) {
+               while (enumerations.hasMoreElements()) {
+                   URL url = enumerations.nextElement();
+                   String protocol = url.getProtocol();
+                   if ("file".equals(protocol)) {
+                       System.out.println(url.getPath());
+                       System.out.println(url.getFile());
+                       printFile(path);
+                   }
+               }
+           }
+       } catch (Exception e) {
+           System.out.println(e.getMessage());
+       }
+    }
+    public static ClassLoader getClassLoader() {
+        return Thread.currentThread().getContextClassLoader();
+    }
+
+}
