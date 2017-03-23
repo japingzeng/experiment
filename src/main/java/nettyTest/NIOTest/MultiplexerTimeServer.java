4cb669f8789ec839feb5c933d624457ab9cd5934
zengjiaping <cdzengjiaping@jd.com>
2017-03-23 14:26:26 +0800

    neety demo

--- src/main/java/nettyTest/NIOTest/MultiplexerTimeServer.java
+++ src/main/java/nettyTest/NIOTest/MultiplexerTimeServer.java
@@ -147,23 +147,4 @@ public class MultiplexerTimeServer implements Runnable{
             new Thread(timeServer, "NIO-MultiplexerTimeServer-001").start();
         }
     }
-
-    /**
-     * @Author japing
-     * @Date 2017/3/17 14:57
-     * @Description:
-     */
-    public static class TimeClient {
-
-        public static void main(String[] args) {
-            int port = 8080;
-            if (null != args && args.length > 0) {
-                port = Integer.valueOf(args[0]);
-            }
-
-            nettyTest.AioTest.TimeClientHandle timeClientHandle = new nettyTest.AioTest.TimeClientHandle("127.0.0.1", port);
-            new Thread(timeClientHandle, "TimeClientHandle").start();
-
-        }
-    }
 }
---
+++ src/main/java/nettyTest/NIOTest/nettyHelloWorld/TimeServerHandler.java
@@ -0,0 +1,43 @@
+package nettyTest.NIOTest.nettyHelloWorld;
+
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+
+/**
+ * @Author japing
+ * @Date 2017/3/20 11:42
+ * @Description:
+ */
+public class TimeServerHandler extends ChannelHandlerAdapter{
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TimeServerHandler.class);
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf)msg;
+        byte[] req = new byte[buf.readableBytes()];
+        buf.readBytes(req);
+        String body = new String(req, "UTF-8");
+        LOGGER.info("the time server receive order: " + body);
+        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.write(resp);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+         ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
